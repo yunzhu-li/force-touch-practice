2d757f005be6b4d28bcc1066f74cf4a80214e479
Yunzhu Li <contact@yunzhu.li>
2015-04-11 22:10:23 -0400

    Add GPLv3 license header to source files

--- FTPractice/FTPractice/src/main/AppDelegate.m
+++ FTPractice/FTPractice/src/main/AppDelegate.m
@@ -1,19 +1,27 @@
 //
-//  AppDelegate.m
-//  FTPractice
+//  This file is part of Force Touch Practice.
 //
 //  Created by Yunzhu Li on 04/10/15.
-//  Copyright (c) 2015 Yunzhu Li. All rights reserved.
+//  Copyright (c) 2015 Yunzhu Li.
+//
+//  Force Touch Practice is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License version 3
+//  as published by the Free Software Foundation.
+//
+//  Force Touch Practice is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with Force Touch Practice.
+//  If not, see http://www.gnu.org/licenses/.
 //
 
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	// Optional: automatically send uncaught exceptions to Google Analytics.
--- FTPractice/FTPractice/src/utils/FPUIUtils.m
+++ FTPractice/FTPractice/src/utils/FPUIUtils.m
@@ -1,9 +1,19 @@
 //
-//  FPUIUtils.m
-//  FTPractice
+//  This file is part of Force Touch Practice.
 //
 //  Created by Yunzhu Li on 04/10/15.
-//  Copyright (c) 2015 Yunzhu Li. All rights reserved.
+//  Copyright (c) 2015 Yunzhu Li.
+//
+//  Force Touch Practice is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License version 3
+//  as published by the Free Software Foundation.
+//
+//  Force Touch Practice is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with Force Touch Practice.
+//  If not, see http://www.gnu.org/licenses/.
 //
 
 #import "FPUIUtils.h"
